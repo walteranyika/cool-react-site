d95443d098e33181dae02f13e3eb78e9ed24842c
Walter Indeche <walter.indeche@andela.com>
2022-09-16 18:19:43 +0300

    Styled About Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import AboutUsPage from "./pages/AboutUsPage";
+import GlobalStyle from "./components/GlobalStyle";
+
 function App() {
-  return (
-    <div className="App">
-      <AboutUsPage/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <GlobalStyle/>
+            <AboutUsPage/>
+        </div>
+    );
 }
 
 export default App;
