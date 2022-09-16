0a701c7840819f4908b0abf2724526d37714e0bb
Walter Indeche <walter.indeche@andela.com>
2022-09-16 22:51:44 +0300

    Styled the FAQ section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+import {About} from "../styles";
 
 const FaqSection =()=>{
     return (
-        <div className="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                <h4>How do I start?</h4>
@@ -10,6 +12,7 @@ const FaqSection =()=>{
                    <p>Lorem ipsum dolor sit amet.</p>
                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, repellat!</p>
                </div>
+                <div className="faq-line"></div>
            </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
@@ -17,6 +20,7 @@ const FaqSection =()=>{
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, repellat!</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>How much does it cost</h4>
@@ -24,6 +28,7 @@ const FaqSection =()=>{
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, repellat!</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What products do you offer</h4>
@@ -31,8 +36,36 @@ const FaqSection =()=>{
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, repellat!</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     )
 }
+
+const Faq = styled(About)`
+   display: block;
+   span{
+     display: block;
+   }
+   h2{
+     padding-bottom: 2rem;
+     font-weight: lighter;
+   }
+   .faq-line{
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0;
+    width: 100%;
+   }
+   .question{
+      padding: 3rem 0;
+      cursor:pointer;
+   }
+   .answer{
+      padding: 2rem 0;
+      p{
+      padding: 1rem 0;
+      }
+   }
+`
 export default FaqSection;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -38,6 +38,7 @@ h3{
 }
 h4 {
     font-weight: bold;
+    font-size: 2rem;
 
 }
 
